114184bfbf0982846fa0e583b907ca52b98d7b2e
SRIHARI <sshsrn@gmail.com>
2022-09-18 16:01:23 +0530

    Updated user profile page
    Signed-off-by: SRIHARI <sshsrn@gmail.com>

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -2,14 +2,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './profile.css';
 import {Link} from "react-router-dom";
 
+// After Setting up Firebase connection, all the data should be pulled from the db
+var link = ""
+function generateImageLink() {
+    link = "https://www.gravatar.com/avatar/"+Math.random().toString(16).slice(2)+"?s=100&d=retro";
+    // Math.random => generates unique alpha numeric string
+    // .toString(16) => generates alpha numeric string containing characters in 0-9 and a-f
+    // .slice(2) => removes first two characters
+}
+generateImageLink();
+
 const Profile = () => {
     return(
         <div className='Profile'>
-        <div className='bods'><Link to='/upload'><p>Upload File</p></Link></div>
-        <div className='History'>
-        <div className='bods_item'><p className='file_name'>File Name</p><Link to='/status'><p>Check Progress</p></Link></div>
-        <div className='bods_item'><p className='file_name'>File Name</p><Link to='/status'><p>Check Progress</p></Link></div>
-        </div>
+            <div className="userProfile p-3">
+                <center>
+                    <h2>User Details</h2>
+                    <img className='profilePic' id='profilePic' src={link} alt="" />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Name:</td>
+                                <td id='user-name'>Test</td>
+                            </tr>
+                            <tr>
+                                <td>Email:</td>
+                                <td id='user-email'>test@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Organisation:</td>
+                                <td id='user-org'>SRMIST</td>
+                            </tr>
+                            <tr>
+                                <td>Role:</td>
+                                <td id='user-role'>Student</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    {/* Update button for updating role (promotion / demotion) */}
+                    {/* <button>Update</button> */}
+                </center>
+            </div>
+            <div className='bods'><Link to='/upload'><p>Upload File</p></Link></div>
+            <div className='History'>
+                <div className='bods_item'><p className='file_name'>File Name</p><Link to='/status'><p>Check Progress</p></Link></div>
+                <div className='bods_item'><p className='file_name'>File Name</p><Link to='/status'><p>Check Progress</p></Link></div>
+            </div>
         </div>
     );
 }
